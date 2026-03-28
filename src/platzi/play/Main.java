f14bc059fbcf78d36f5cf12b1c8c4e729bc42d6e
DiegoBadillo <diegoBadillo@live.com>
2026-03-28 00:04:15 -0600

    Clase 4 Funcion Scanner

--- src/platzi/play/Main.java
+++ src/platzi/play/Main.java
@@ -1,7 +1,20 @@
 package platzi.play;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo ");
+        System.out.println("Platzi Play");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Cual es tu nombre?");
+        String nombre = scanner.nextLine();
+
+        System.out.println("Hola " + nombre + ", esto es Platzi Play");
+
+        System.out.println(nombre + " cuantos años tienes?");
+        int edad = scanner.nextInt();
+
+        System.out.println(nombre + " puede ver contenido + " + edad);
     }
 }
